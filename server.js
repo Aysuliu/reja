21acc4b19c8b3a1459aaa797f4452623ec94d6b8
Aysulu <bakhievaays@gmail.com>
2025-12-23 17:48:43 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,29 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+//1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//2 Session code
+//3 Views backend server site rendering
+app.set("views", "views");
+app.set("view engine", "ejs");
+//single page appplication method
+
+//4 Routing code
+app.get("/hello", function(req, res) {
+    res.end(`<h1 style="background-color: red"> Hello WOrld </h1>`); // html faylni render qilish
+});
+app.get("/gift", function(req, res) {
+    res.end(`<h1> Siz sovg'alar bolimidasiz </h1>`); // html faylni render qilish
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is running successfully on port: ${PORT}`);
+});
