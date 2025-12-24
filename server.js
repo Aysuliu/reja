4e80f257dd4451923c27b21eefeb8a5228498096
Aysulu <bakhievaays@gmail.com>
2025-12-24 18:53:05 +0900

    add: ejs file

--- server.js
+++ server.js
@@ -9,21 +9,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //2 Session code
-//3 Views backend server site rendering
+//3 BSSR => Views backend server site rendering
 app.set("views", "views");
 app.set("view engine", "ejs");
 //single page appplication method
 
 //4 Routing code
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style="background-color: red"> Hello WOrld </h1>`); // html faylni render qilish
+// app.get("/hello", function(req, res) {
+//     res.end(`<h1 style="background-color: red"> Hello WOrld </h1>`); // html faylni render qilish
+// });
+// app.get("/gift", function(req, res) {
+//     res.end(`<h1> Siz sovg'alar bolimidasiz </h1>`); // html faylni render qilish
+// });
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
 });
-app.get("/gift", function(req, res) {
-    res.end(`<h1> Siz sovg'alar bolimidasiz </h1>`); // html faylni render qilish
-});
+
+app.get("/", function(req, res) {
+    res.render('harid.ejs')
+})
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
     console.log(`The server is running successfully on port: ${PORT}`);
 });
+
+//Client-side rendering => React
