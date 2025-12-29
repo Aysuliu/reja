1db8374e7dbc9d0c5c74bcb315d6365e95e98dcf
Aysulu <bakhievaays@gmail.com>
2025-12-29 15:09:37 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -42,14 +42,14 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function(req, res) {
-    res.render('harid.ejs')
+    res.render('reja.ejs')
 })
 
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}`);
+    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
 
 //Client-side rendering => React
